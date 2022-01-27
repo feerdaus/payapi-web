5fe840c69964a856f9babf756afbf5d8b80a604e
feerdaus <firdauslawal@gmail.com>
2022-01-27 09:52:04 +0100

    add color palette

---
+++ src/Theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    common: {
+      secondary_pink: "#da6d97",
+    },
+    primary: {
+      main: "#ba4270",
+      light: "#fbfcfe",
+    },
+    secondary: {
+      main: "#36536b",
+      dark: "#1b262f",
+      light: "#6c8294",
+    },
+  },
+});
+
+export default theme;
